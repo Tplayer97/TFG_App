ca10a3d8319ad9ef8c4c249e9c1be13249297ba8
unknown <jose.moreno063@alu.uhu.es>
2022-03-21 19:10:21 +0100

    piezo con la db

---
+++ src/Entity/Admin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdminRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AdminRepository::class)
+ */
+class Admin
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+//esta es la id
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $Nombre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->Nombre;
+    }
+
+    public function setNombre(string $Nombre): self
+    {
+        $this->Nombre = $Nombre;
+
+        return $this;
+    }
+}
