0ffb9c9641bbce07e8c75ea8271686ba175776c6
unknown <jose.moreno063@alu.uhu.es>
2022-04-20 19:57:32 +0200

    base ttemplate added

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+class DefaultController extends AbstractController
+{
+/**
+ *  @Route("/", name="index")
+ */
+public function test(){
+
+
+    dd("hola");
+}
+}
